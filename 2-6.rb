025a1a57401e2be1d19b542638f941280f24d091
imarinxx <lotvt.bw.1940@gmail.com>
2020-01-09 13:39:14 +0900

    2-6

---
+++ 2-6.rb
@@ -0,0 +1,20 @@
+puts 100
+puts 100 + 5
+
+puts "Rudyを学習"
+# 出力結果 = "Rudyを学習"
+name = "鈴木さん"
+puts "Hello" + name
+# 出力結果 = Hello鈴木さん
+puts "こんにちは#{name}"
+# 出力結果=>こんにちは鈴木さん
+
+names = ["Perl", "Ruby", "Python", "Swift"]
+puts names[1]
+# 出力結果 Ruby
+tall = {"太郎"=>185, "二郎"=>170, "花子"=>150}
+puts tall["太郎"]
+# 出力結果=>185
+tall = {:太郎=>185, :二郎=>170, :花子=>150}
+puts tall[:花子
+# 出力結果=>150
