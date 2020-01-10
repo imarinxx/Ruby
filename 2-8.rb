f2627f0424d817c8fd6997c3b944169527f515ad
imarinxx <lotvt.bw.1940@gmail.com>
2020-01-10 19:14:38 +0900

    2-8

--- 2-8.rb
+++ 2-8.rb
@@ -1,7 +1,7 @@
 i = 0
 while i <= 10 do
 	if i >5
-		break #iが5より大きくなると繰り返しから抜ける
+	break #iが5より大きくなると繰り返しから抜ける
 	end
 	puts i
 	i += 1
--- lesson7.rb
+++ lesson7.rb
@@ -1,6 +1,5 @@
 puts "計算を始めます"
 puts "2つの値を入力してください"
- puts
   a=gets.to_i
   b=gets.to_i
   puts "計算結果を出力します"
@@ -9,7 +8,18 @@ puts "2つの値を入力してください"
 
 puts "計算を始めます"
 puts "何回計算を繰り返しますか？"
-  puts
   i=gets.to_i
-  while i == 0 do
- 	puts "#{i-(i-1)}回目の目の計算"
+  for j in 1..i do
+ 		puts "#{j}回目の計算"
+ 		puts "2つの値を入力してください"
+ 		 c=gets.to_i
+  		 d=gets.to_i
+ 			puts "c=#{c}"
+ 			puts "d=#{d}"
+ 			puts "計算結果を出力します"
+ 			puts "c+d=#{c+d}"
+ 			puts "c-d=#{c-d}"
+ 			puts "c*d=#{c*d}"
+ 			puts "c/d=#{c/d}"
+ 		end
+ 		puts "計算を終了します"
